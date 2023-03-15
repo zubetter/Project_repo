990beeff877a388327930681002e8e3a33e2843a
zubetter <115677943+zubetter@users.noreply.github.com>
2023-03-16 02:05:42 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
-
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "us-east-2"
+}
+variable "access_key" {
+  default = ""
+}
+variable "secret_key" {
+    default = ""
 }
